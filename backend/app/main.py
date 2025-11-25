867dc97b44977339cecde9721f1786c520e53679
DulinNethmira <dulinethmira08@gmail.com>
2025-11-25 11:59:10 +0530

    Updated backend routes and database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.api.routes import auth, events, booking, wishlist, support, admin
 
-app = FastAPI()
+app = FastAPI(title="Event Management System")
+
+app.include_router(auth.router)
+app.include_router(events.router)
+app.include_router(booking.router)
+app.include_router(wishlist.router)
+app.include_router(support.router)
+app.include_router(admin.router)
 
 @app.get("/")
 def root():
-    return {"message": "Backend is running"}
+    return {"message": "ESMS Backend Running"}
