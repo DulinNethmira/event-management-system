29a3aab2bcbf194fe30366e8ce8383d1a44d3ce3
DulinNethmira <dulinethmira08@gmail.com>
2025-11-25 17:44:12 +0530

    config.py change and add DATABASE_URL for .env

--- backend/app/api/core/database.py
+++ backend/app/api/core/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from core.config import settings
 
-DATABASE_URL = "postgresql://ems_user:11Lega**11@localhost:5432/ems-db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
