ca454b286d74fcd3c67bb3dce298c37f8c7c9cad
rehan-weliwatta <thiwankarehan@gmail.com>
2025-12-20 20:08:43 +0530

    set up JWT Auth and Google Facebook authentication

---
+++ backend/app/api/routes/auth.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, Request
+from backend.app.api.core.security import oauth
+
+router = APIRouter()
+
+#route to handle Google OAuth
+@router.get("/auth/google")
+async def auth_google(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+    return {"user": user}
+
+# Route to handle Facebook OAuth
+@router.get("/auth/facebook")
+async def auth_facebook(request: Request):
+    token = await oauth.facebook.authorize_access_token(request)
+    user = await oauth.facebook.parse_id_token(request, token)
+    return {"user": user}
